69ff4f4a463d114a8dfce1b03c935bc8ebde752b
Alex Brilevski <brilevski.alex@gmail.com>
2026-05-26 23:48:40 +0300

    Initial setup of basic server

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,30 +1,9 @@
-const fs = require('fs');
+const http = require('http');
 
-// Blocking, synchronous way
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textInput);
-
-const textOutput = `This is what we know about avocado: ${textInput}.\nCreated on ${new Date().toLocaleDateString()}`;
-fs.writeFileSync('./txt/output.txt', textOutput);
-console.log('Text output file written.');
-
-// Non-blocking, asynchronous way
-fs.readFile('./txt/start.txt', 'utf-8', (error, fileName) => {
-  if (error) {
-    console.log('Error! An error occured when reading file.');
-    return;
-  }
-
-  fs.readFile(`./txt/${fileName}.txt`, 'utf-8', (error, text) => {
-    console.log(text);
-
-    fs.readFile('./txt/append.txt', 'utf-8', (error, append) => {
-      console.log(append);
-
-      fs.writeFile('./txt/final.txt', `${text}\n${append}`, error => {
-        console.log('The final file has been written successfully.');
-      });
-    });
-  });
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server!');
 });
-console.log('Will read file asynchronously');
+
+server.listen(8000, 'localhost', () => {
+  console.log('Listening of requests on localhost, port 8000');
+});
