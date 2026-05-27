9ec03660ff0228690bd845bf32f7456c31b7b90a
Alex Brilevski <brilevski.alex@gmail.com>
2026-05-27 23:26:32 +0300

    Basic routing added

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,7 +1,18 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello from the server!');
+  const route = req.url;
+
+  if (route === '/' || route === '/overview') {
+    res.end('This is Overview page');
+  } else if (route === '/product') {
+    res.end('This is Product page');
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+    });
+    res.end('<h1>Page not found</h1>');
+  }
 });
 
 server.listen(8000, 'localhost', () => {
