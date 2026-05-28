95cd90091a8c88944fc591ae4264e0874612ad77
Alex Brilevski <brilevski.alex@gmail.com>
2026-05-28 23:24:08 +0300

    Simple API for products data was added

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const http = require('http');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   const route = req.url;
@@ -7,6 +11,11 @@ const server = http.createServer((req, res) => {
     res.end('This is Overview page');
   } else if (route === '/product') {
     res.end('This is Product page');
+  } else if (route === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
