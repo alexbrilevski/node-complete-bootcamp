1837a2afacfc737106bd94b0d0ad6d185dd2f432
Alex Brilevski <brilevski.alex@gmail.com>
2026-05-31 20:44:21 +0300

    Ids in product page URL were replaced with slugs

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const slugify = require('slugify');
 const replaceTemplateVariables = require('./modules/replaceTemplateVariables');
 
 const cardTemplate = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -9,20 +10,22 @@ const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overvi
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const productData = dataObj.map(el => ({ ...el, slug: slugify(el.productName, { lower: true }) }));
 
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
+  const pathSegments = pathname.split('/').filter(el => el !== '');
 
   if (pathname === '/' || pathname === '/overview') {
-    const cardsHtml = dataObj.map(el => replaceTemplateVariables(cardTemplate, el)).join('');
+    const cardsHtml = productData.map(el => replaceTemplateVariables(cardTemplate, el)).join('');
     const output = overviewTemplate.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
 
     res.writeHead(200, {
       'Content-type': 'text/html',
     });
     res.end(output);
-  } else if (pathname === '/product') {
-    const product = dataObj.find(el => el.id === Number(query.id));
+  } else if (pathSegments[0] === 'product' && pathSegments[1]) {
+    const product = productData.find(el => el.slug === pathSegments[1]);
     let output = '';
 
     if (product) {
--- node-farm/modules/replaceTemplateVariables.js
+++ node-farm/modules/replaceTemplateVariables.js
@@ -8,7 +8,7 @@ module.exports = (template, product) => {
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%SLUG%}/g, product.slug);
     output = output.replace(/{%NOT_ORGANIC%}/g, notOrganicClassName);
 
     return output;
