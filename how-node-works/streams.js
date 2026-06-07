ab7e92073268e5b2dd5e17fb207eb2089851a3ae
Alex Brilevski <brilevski.alex@gmail.com>
2026-06-07 14:59:40 +0300

    Readable and writable streams example

---
+++ how-node-works/streams.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  // Solution 1
+  // fs.readFile('test-file.txt', 'utf-8', (error, data) => {
+  //   if (error) console.log(error);
+  //   res.end(data);
+  // });
+
+  // Solution 2: Streams
+  // const readable = fs.createReadStream('test-file.txt');
+
+  // readable.on('data', chunk => {
+  //   res.write(chunk);
+  // });
+
+  // readable.on('end', () => {
+  //   res.end();
+  // });
+
+  // readable.on('error', error => {
+  //   console.log(error);
+  //   res.statusCode = 500;
+  //   res.end('File not found');
+  // });
+
+  // Solution 3: Streams with pipe
+  // readableStreamSource.pipe(writableStreamDestination)
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res);
+});
+
+server.listen(8000, 'localhost', () => {
+  console.log('Listening to requests...');
+});
