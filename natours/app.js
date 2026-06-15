a0ef4d383a44287922760f4946454f831a2b43e2
Alex Brilevski <brilevski.alex@gmail.com>
2026-06-15 21:55:00 +0300

    Endpoint for fetching simple tours data was added

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,9 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
 app.get('/', (request, response) => {
   response.status(200).json({ message: 'Test message', app: 'Natours' });
@@ -8,6 +11,16 @@ app.get('/', (request, response) => {
 
 app.post('/', (request, response) => {
   response.status(200).send('Post requests to this endpoint are allowed');
+});
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 const port = 3000;
