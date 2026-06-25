c471484f129c043179bf6bc132962ea9d6758063
Alex Brilevski <brilevski.alex@gmail.com>
2026-06-25 19:51:08 +0300

    Middleware for serving static files from public directory was added

--- natours/app.js
+++ natours/app.js
@@ -11,6 +11,8 @@ app.use(morgan('dev'));
 // Use middleware to add body property to the request object
 app.use(express.json());
 
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log('Middleware 1 executed');
   next();
