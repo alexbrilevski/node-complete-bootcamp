1969139423ecb59fa2d70fc7b2d7b82255b6c054
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-06 19:19:29 +0300

    Database connection setup was added

--- natours/server.js
+++ natours/server.js
@@ -1,7 +1,20 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Database connection successful');
+  });
 
 // Start server
 const port = process.env.PORT || 8000;
