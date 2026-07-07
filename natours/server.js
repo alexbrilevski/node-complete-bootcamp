cceb0061a74acd7de98c42660c24d3bdb650a411
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-07 21:17:32 +0300

    Creating a test tour in a data base was added

--- natours/server.js
+++ natours/server.js
@@ -34,6 +34,21 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
+
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
