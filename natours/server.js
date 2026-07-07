25cc3843b71cf77878678dd6c1dc984ca139985b
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-07 19:53:32 +0300

    Tour schema and model were added

--- natours/server.js
+++ natours/server.js
@@ -16,6 +16,24 @@ mongoose
     console.log('Database connection successful');
   });
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
 // Start server
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
