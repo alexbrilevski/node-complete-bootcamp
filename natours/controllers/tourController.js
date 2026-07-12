e6d75e01e75701d63b82392cf838cdb51f8fe090
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-12 12:34:29 +0300

    Excluding query parameters from a request query was implemented

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,14 +2,21 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
-    // const tours = await Tour.find()
+    // Build query
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
+    const query = Tour.find(queryObj);
+    // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-    const tours = await Tour.find(req.query);
 
+    // Execute query
+    const tours = await query;
+
+    // Send sesponse
     res.status(200).json({
       status: 'success',
       requestedAt: req.requestTime,
