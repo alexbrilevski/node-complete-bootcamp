45bbbad4794df8f7046c881f19dd18a5cf668076
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-13 14:44:16 +0300

    Alias route was added for fetching top 5 tours

--- natours/routes/tourRoutes.js
+++ natours/routes/tourRoutes.js
@@ -7,6 +7,9 @@ router.route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
 
+router.route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
 router.route('/:id')
   .get(tourController.getTour)
   .patch(tourController.updateTour)
