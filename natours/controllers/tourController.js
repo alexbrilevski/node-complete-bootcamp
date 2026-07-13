15b966da994adfa6b2af452f6c3e401e9ee45e39
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-13 13:26:08 +0300

    Request results pagination was added to tours controller

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -39,6 +39,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select('-__v');
     }
 
+    // 5) Pagination
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const totalCount = await Tour.countDocuments();
+
+      if (skip >= totalCount) {
+        throw new Error('This page does not exist');
+      }
+    }
+
     // Execute query
     const tours = await query;
 
