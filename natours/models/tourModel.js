bd83d17f2a544e3ec4410a8d8206dcd9b8ced1ae
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-15 18:08:31 +0300

    Query middleware was added to tour model to process the secret tour property

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -54,6 +54,10 @@ const tourSchema = new mongoose.Schema({
     select: false,
   },
   startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true },
@@ -78,6 +82,19 @@ tourSchema.pre('save', function (next) {
 //   console.log('Saved document:', doc);
 //   next();
 // });
+
+// Query middleware
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
+  next();
+});
 
 const Tour = mongoose.model('Tour', tourSchema);
 
