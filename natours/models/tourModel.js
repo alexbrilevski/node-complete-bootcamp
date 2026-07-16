9e5239110ceb94ea21ef7e42127001f0ff691f12
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-16 10:56:28 +0300

    Aggregation middleware was added to tour model to exclude secret tours from aggregation operations

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -90,9 +90,16 @@ tourSchema.pre(/^find/, function (next) {
   next();
 });
 
-tourSchema.post(/^find/, function(docs, next) {
+tourSchema.post(/^find/, function (docs, next) {
   console.log(`Query took ${Date.now() - this.start} milliseconds`);
   console.log(docs);
+  next();
+});
+
+// Aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
