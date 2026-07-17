fb76178961f513cfdc6e5b6d302397a8a2ea218a
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-17 11:44:13 +0300

    Undefined routes handler for all request types was added

--- natours/app.js
+++ natours/app.js
@@ -29,4 +29,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Undefined routes handler for all request types
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server.`,
+  });
+  next();
+});
+
 module.exports = app;
