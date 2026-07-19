90ebc0be3ad8b807f8a2e4d5b67bebd45ef86c2b
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-19 16:34:56 +0300

    Handling 404 Error in asyncronoys functions was added

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,6 +2,7 @@ const { get } = require('mongoose');
 const Tour = require('./../models/tourModel');
 const APIFeatures = require('./../utils/apiFeatures');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('../utils/appError');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -35,6 +36,10 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
 exports.getTour = catchAsync(async (req, res, next) => {
   // Tour.findOne({ _id: req.params.id });
   const tour = await Tour.findById(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour was found with that Id', 404));
+  }
 
   res.status(200).json({
     status: 'success',
@@ -138,6 +143,10 @@ exports.updateTour = catchAsync(async (req, res, next) => {
     runValidators: true,
   });
 
+  if (!tour) {
+    return next(new AppError('No tour was found with that Id', 404));
+  }
+
   res.status(200).json({
     status: 'success',
     data: {
@@ -147,7 +156,11 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  await Tour.findByIdAndDelete(req.params.id);
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) {
+    return next(new AppError('No tour was found with that Id', 404));
+  }
 
   res.status(204).json({
     status: 'success',
