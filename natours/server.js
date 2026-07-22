3f47c6526413bbee79063a85b780e1c04e2b3c4d
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-22 18:34:57 +0300

    Catching and handling all unhandled promise rejections in the app was added

--- natours/server.js
+++ natours/server.js
@@ -18,6 +18,14 @@ mongoose
 
 // Start server
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on local host, port ${port}`);
+});
+
+process.on('unhandledRejection', error => {
+  console.log(error.name, error.message);
+  console.log('Unhandled Rejection. Shutting down..');
+  server.close(() => {
+    process.exit(1);
+  });
 });
