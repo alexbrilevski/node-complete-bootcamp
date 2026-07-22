570666915cfbce599558be2e6fae1536d446cf37
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-22 18:49:51 +0300

    Catching and handling uncaught exceptions in the app was added

--- natours/server.js
+++ natours/server.js
@@ -2,6 +2,12 @@ const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', error => {
+  console.log('Uncaught exception. Shutting down..');
+  console.log(error.name, error.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DATABASE_LOCAL;
@@ -23,8 +29,8 @@ const server = app.listen(port, () => {
 });
 
 process.on('unhandledRejection', error => {
-  console.log(error.name, error.message);
   console.log('Unhandled Rejection. Shutting down..');
+  console.log(error.name, error.message);
   server.close(() => {
     process.exit(1);
   });
