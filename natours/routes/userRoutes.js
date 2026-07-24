ace1fa6dcc4056f7d0d5ea47165bb46aa0928cbc
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-24 20:36:36 +0300

    Basic logic for creating users was implemented (authentication controller and route)

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userControlller = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
 
 router.route('/')
   .get(userControlller.getAllUsers)
