d442e8ea8a7b579a5b091a9a2f7124f6208afa01
Alex Brilevski <brilevski.alex@gmail.com>
2026-07-30 19:44:46 +0300

    Authorization logic was added (User roles and permissions)

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -79,3 +79,11 @@ exports.protectRoute = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo = (...roles) => (req, res, next) => {
+  if (!roles.includes(req.user.role)) {
+    return next(new AppError('You do not have permission to perform this action', 403));
+  }
+
+  next();
+};
