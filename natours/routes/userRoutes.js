29701bcea4ee96106641471d8eb90e247df88ba0
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-04 18:30:42 +0300

    A typo in user controller variable name was corrected

--- natours/routes/userRoutes.js
+++ natours/routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userControlller = require('./../controllers/userController');
+const userController = require('./../controllers/userController');
 const authController = require('./../controllers/authController');
 
 const router = express.Router();
@@ -10,12 +10,12 @@ router.post('/forgot-password', authController.forgotPassword);
 router.patch('/reset-password/:token', authController.resetPassword);
 
 router.route('/')
-  .get(userControlller.getAllUsers)
-  .post(userControlller.createUser);
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router.route('/:id')
-  .get(userControlller.getUser)
-  .patch(userControlller.updateUser)
-  .delete(userControlller.deleteUser);
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
