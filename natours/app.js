539981b672d061f05b4f0bc515e45743a51443ff
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-06 23:12:21 +0300

    Setting response security HTTP headers was added

--- natours/app.js
+++ natours/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -9,10 +11,15 @@ const globalErrorHandler = require('./controllers/errorController');
 const app = express();
 
 // Global Middlewares
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from the same IP
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -20,16 +27,17 @@ const limiter = rateLimit({
 });
 app.use('/api', limiter);
 
-// Use middleware to add body property to the request object
-app.use(express.json());
+// Body parser, reading data from request body into req.body
+app.use(express.json({ limit: '10kb' }));
 
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   console.log('Middleware 1 executed');
   next();
 });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
