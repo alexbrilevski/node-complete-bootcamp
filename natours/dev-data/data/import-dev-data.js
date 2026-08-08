d15ac201a161cc73fda64d2c4fb2753a308ba37f
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-08 11:40:46 +0300

    Locations modeling (Geospatial data) was added to the tour model

--- natours/dev-data/data/import-dev-data.js
+++ natours/dev-data/data/import-dev-data.js
@@ -18,7 +18,7 @@ mongoose
   });
 
 // Read data from JSON file
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // Import data into DB
 const importData = async () => {
