97477d3dccaa11def99bf99a66a7aafaded8ad8e
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-10 21:46:36 +0300

    User data filtering in tour guides populating logic was fixed

--- natours/models/tourModel.js
+++ natours/models/tourModel.js
@@ -148,7 +148,7 @@ tourSchema.pre(/^find/, function (next) {
 tourSchema.pre(/^find/, function (next) {
   this.populate({
     path: 'guides',
-    select: '-__v, -passwordChangedAt',
+    select: '-__v -passwordChangedAt',
   });
   next();
 });
