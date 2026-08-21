a0e8c57b2360d6b7f01a173882eb3ba9242d25c5
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-21 23:02:53 +0300

    Extending the base template content block was implemented

--- natours/app.js
+++ natours/app.js
@@ -72,8 +72,19 @@ app.use((req, res, next) => {
 // Routes
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Alex',
+    title: 'Exciting tours for adventurous people',
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All tours',
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker',
   });
 });
 
