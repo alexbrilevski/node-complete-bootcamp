661aef93e162d590bbfa8e9fa61a929aa5bcd4c6
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-22 15:55:51 +0300

    Tour details page was implemented

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -5,15 +5,23 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
 
-  // 2) Render template using tour data
+  // 2) Render template using tours data
   res.status(200).render('overview', {
     title: 'Exciting tours for adventurous people',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get data for the requested tour (including guides and reviews)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+
+  // 2) Render template using tour data
+  res.status(200).render('tour', {
+    title: tour.name,
+    tour,
+  });
+});
