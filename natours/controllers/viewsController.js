bf67c2bb715f69902a8ab5111f35366bd458d607
Alex Brilevski <brilevski.alex@gmail.com>
2026-08-22 11:34:06 +0300

    Tour listing page was implemented

--- natours/controllers/viewsController.js
+++ natours/controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) Get tour data from collection
+  const tours = await Tour.find();
+
+  // 2) Render template using tour data
   res.status(200).render('overview', {
     title: 'Exciting tours for adventurous people',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
