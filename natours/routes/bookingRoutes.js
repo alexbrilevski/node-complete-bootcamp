47021ee0121243e818e7602f8e05150e118f4724
Alex Brilevski <brilevski.alex@gmail.com>
2026-09-06 14:59:01 +0300

    Implementation of Bookings API was completed

--- natours/routes/bookingRoutes.js
+++ natours/routes/bookingRoutes.js
@@ -4,10 +4,22 @@ const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
+router.use(authController.protectRoute);
+
 router.get(
   '/checkout-session/:tourId',
-  authController.protectRoute,
   bookingController.getCheckoutSession
 );
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router.route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking)
+
+router.route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.updateBooking)
+  .delete(bookingController.deleteBooking);
 
 module.exports = router;
