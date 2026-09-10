6197548ebffb547934e579bf2226bc2834fdb59a
Alex Brilevski <brilevski.alex@gmail.com>
2026-09-10 20:36:11 +0300

    CORS was implemented

--- natours/app.js
+++ natours/app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -25,6 +26,18 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // Global Middlewares
+// Allow CORS from all domains
+app.use(cors());
+
+// api.natours.com, front-end natours.com
+// app.use(cors({
+//   origin: 'https://www.natours.com'
+// }));
+
+// Respond to OPTIONS requests on all routs
+app.options('*', cors());
+// app.options('api/v1/tours/:id', cors());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
